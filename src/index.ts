088956485041906d5b38e6a831ba763a5a6cf381
Jake <dominik8655@gmail.com>
2023-10-11 19:53:53 +0200

    feat: read from stdin using fs.readFileSync

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+import fs from 'fs';
+
+const input = fs.readFileSync('/dev/stdin').toString();
+const lines = input.split('\n');
+const a = parseInt(lines[0]!);
+const b = parseInt(lines[1]!);
+
+console.log(+a + +b);
