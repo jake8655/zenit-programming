a8920b213ea87279b7d9f6c00f0148c1cedd0fee
Jake <dominik8655@gmail.com>
2023-10-11 19:52:28 +0200

    feat: read from stdin using process.stdin.ondata

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+process.stdin.setEncoding('utf8');
+
+import { readlineSync } from './utils';
+
+const main = async () => {
+  const a = await readlineSync();
+  const b = await readlineSync();
+  console.log(+a + +b);
+};
+
+main();
