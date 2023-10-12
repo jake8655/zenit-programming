6b3012ab4ce8798df719609dc7c809151812f495
Jake <dominik8655@gmail.com>
2023-10-12 20:39:37 +0200

    feat: solve zenit17cka

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,28 @@ process.stdin.setEncoding('utf8');
 
 import { readlineSync } from './utils';
 
+const convert = {
+  Vlavo: 'Dolava',
+  Vpravo: 'Doprava',
+};
+
 const main = async () => {
-  const a = await readlineSync();
-  const b = await readlineSync();
-  console.log(+a + +b);
+  const input = await readlineSync();
+  const sentences = input
+    .split(/\.\ /s)
+    .map(sentence => sentence.replace('.', ''));
+  const advices = sentences
+    .map(sentence => {
+      const words = sentence.split(' ');
+      return [
+        convert[words[0] as keyof typeof convert],
+        words[words.length - 1],
+      ];
+    })
+    .filter(advice => advice[1] !== 'drak') as [string[]];
+
+  const output = advices[0].join(', lebo je tam ');
+  console.log(`${output}!`);
 };
 
 main();
