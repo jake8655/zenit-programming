2a423513786b9550dadbf31c36a9bd6d292fe5f7
Jake <dominik8655@gmail.com>
2023-10-13 16:41:27 +0200

    feat: solve zenit22skb

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ process.stdin.setEncoding('utf8');
 import { readlineSync } from './utils';
 
 const main = async () => {
-  const a = await readlineSync();
-  const b = await readlineSync();
-  console.log(+a + +b);
+  const numberOfMoves = +(await readlineSync());
+  let final = 0;
+
+  for (let i = 0; i < numberOfMoves; i++) {
+    const move = +(await readlineSync());
+    final += move;
+  }
+
+  console.log(final);
 };
 
 main();
