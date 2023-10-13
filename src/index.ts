5808c3fee57610ecf1b147755ad0afe6db5fb4b6
Jake <dominik8655@gmail.com>
2023-10-13 16:32:23 +0200

    feat: solve zenit22ska

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-process.stdin.setEncoding('utf8');
-
-import { readlineSync } from './utils';
-
-const main = async () => {
-  const a = await readlineSync();
-  const b = await readlineSync();
-  console.log(+a + +b);
-};
-
-main();
+console.log(4);
