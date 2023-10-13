38ab78a14509ab9c4d15a0da9506ea77461eb5ae
Jake <dominik8655@gmail.com>
2023-10-13 22:11:48 +0200

    feat solve zenit17skb

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,19 @@ import fs from 'fs';
 
 const input = fs.readFileSync('/dev/stdin').toString();
 const lines = input.split('\n');
-const a = parseInt(lines[0]!);
-const b = parseInt(lines[1]!);
 
-console.log(+a + +b);
+const [player1, player2] = lines[0]!.split(' ').map(num => +num);
+
+const nums = lines[2]!.split(' ').map(num => +num);
+const [player1Score, player2Score] = nums.reduce(
+  (acc, cur) => {
+    if (cur === player1) acc[0]++;
+    if (cur === player2) acc[1]++;
+    return acc;
+  },
+  [0, 0],
+);
+
+if (player1Score > player2Score) console.log('Jergus');
+else if (player1Score < player2Score) console.log('Ema');
+else console.log('remiza');
