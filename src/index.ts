d64b76a38ffc965784a6685306eee54837b1a42c
Dominik Toth <dominik8655@gmail.com>
2023-10-17 09:30:07 +0200

    feat: partially solve zenit23skc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import fs from 'fs';
 
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-const a = parseInt(input[0]!);
-const b = parseInt(input[1]!);
+const costs = input[1]!.split(' ').map(val => +val);
 
-console.log(+a + +b);
+const result = costs.reduce((acc, _, idx) => {
+  const newArr = [...costs];
+  newArr.splice(idx, 1);
+  acc.push(newArr.reduce((acc2, val2) => acc2 + val2, 0));
+  return acc;
+}, new Array<number>());
+
+console.log(result.join('\n'));
