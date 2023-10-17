fe2d92198c1a2e0927b9fd3776860657a870a61e
Dominik Toth <dominik8655@gmail.com>
2023-10-17 08:21:28 +0200

    refactor: split on one line

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import fs from 'fs';
 
-const input = fs.readFileSync('/dev/stdin').toString();
-const lines = input.split('\n');
-const a = parseInt(lines[0]!);
-const b = parseInt(lines[1]!);
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const a = parseInt(input[0]!);
+const b = parseInt(input[1]!);
 
 console.log(+a + +b);
