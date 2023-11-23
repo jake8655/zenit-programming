abfefe67f9f88a1c3f930221d8251abf04a8139a
Dominik Toth <dominik8655@gmail.com>
2023-11-23 11:02:15 +0100

    feat: solve zenit23kke

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,42 @@
 import fs from 'fs';
 
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-const a = parseInt(input[0]!);
-const b = parseInt(input[1]!);
+const [, flavorsPerDay] = input[0]!.split(' ').map(val => +val) as [
+  number,
+  number,
+];
+const likeness = input[1]!.split(' ').map(val => +val);
 
-console.log(+a + +b);
+let groups: number[][] = [];
+
+if (likeness.length % flavorsPerDay === 0) {
+  for (let i = 0; i < likeness.length; i++) {
+    const group: number[] = [];
+    for (let j = i; j < i + flavorsPerDay; j++) {
+      group.push(likeness[j]!);
+    }
+  }
+} else {
+  for (let i = 0; i < likeness.length; i++) {
+    const group: number[] = [];
+    for (let j = i; j < i + flavorsPerDay; j++) {
+      if (j > likeness.length) {
+        group.push(likeness[j - likeness.length]!);
+        break;
+      }
+      group.push(likeness[j]!);
+    }
+    groups.push(group);
+  }
+}
+
+groups = groups.filter(group => {
+  group = group.filter(Boolean);
+  return group.length === flavorsPerDay;
+});
+
+const maxGroups = groups.map(group => {
+  return Math.max(...group);
+});
+
+console.log(maxGroups.join(' '));
