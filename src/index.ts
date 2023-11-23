55a2fe138aa5c8bf25a09d1495830a3f3791d9bb
Dominik Toth <dominik8655@gmail.com>
2023-11-23 09:30:37 +0100

    feat: solve zenit23kkb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import fs from 'fs';
 
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-const a = parseInt(input[0]!);
-const b = parseInt(input[1]!);
+const replacementNames = input[1]!.split(' ');
+const sheepArr = input
+  .slice(2)
+  .map(row => row.split('').map(val => +val || '.')) as (number | string)[][];
 
-console.log(+a + +b);
+const renamedSheep = sheepArr.map(row => {
+  return row.map(val => {
+    if (val === '.') {
+      return '.';
+    }
+    return replacementNames[(val as number) - 1];
+  });
+});
+
+const result = renamedSheep.map(row => row.join('')).join('\n');
+console.log(result);
