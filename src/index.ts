bc3d05f80342452224f1879278b989e260a8d45d
Dominik Toth <dominik8655@gmail.com>
2023-11-23 09:53:38 +0100

    feat: solve zenit23kkc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
 import fs from 'fs';
 
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-const a = parseInt(input[0]!);
-const b = parseInt(input[1]!);
+const shirts = input[1]!.split(' ');
 
-console.log(+a + +b);
+const combinations: number[] = [];
+
+for (let i = 0; i < shirts.length; i++) {
+  const different = new Set<string>();
+  const sequence: string[] = [shirts[i]!];
+
+  for (let j = i + 1; j < shirts.length; j++) {
+    if (different.size > 2) {
+      different.clear();
+      sequence.pop();
+      break;
+    }
+
+    if (shirts[i] === shirts[j]) {
+      different.add(shirts[i]!);
+    }
+
+    different.add(shirts[i]!);
+    different.add(shirts[j]!);
+
+    sequence.push(shirts[j]!);
+  }
+
+  if (different.size <= 2) {
+    combinations.push(sequence.length);
+  }
+}
+
+const maxCombination = Math.max(...combinations);
+
+console.log(maxCombination);
