5a5c9756d23355621d0488e55bad2a56abbf1641
Dominik Toth <dominik8655@gmail.com>
2023-11-23 13:33:20 +0100

    feat: solve zenit23kki

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import fs from 'fs';
 
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
-const a = parseInt(input[0]!);
-const b = parseInt(input[1]!);
+input.shift();
+input.pop();
+const menus = input.map(line => line.split(' ').map(num => +num));
 
-console.log(+a + +b);
+const firstMenu = [...menus[0]!];
+const secondMenu = [...menus[0]!];
+
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < menus.length; j++) {
+    if (firstMenu[i]! < menus[j]![i]!) {
+      firstMenu[i] = menus[j]![i]!;
+    }
+    if (secondMenu[i]! > menus[j]![i]!) {
+      secondMenu[i] = menus[j]![i]!;
+    }
+  }
+}
+
+const [first, second] = [firstMenu, secondMenu].map(menu =>
+  menu.reduce((acc, cur) => acc + cur, 0),
+) as [number, number];
+
+console.log(first - second);
